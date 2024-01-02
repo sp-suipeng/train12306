806ec9dd23840704699e87bf252b76a788e5b99b
nbsuipeng <1547837674@qq.com>
2024-01-02 21:30:06 +0800

    创建登录路由及界面

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="">
+    <LoginView></LoginView>
   </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LoginView from "@/views/LoginView.vue";
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    LoginView
   }
 }
 </script>
+
+<style>
+.login_middle {
+
+}
+</style>
---
+++ web/src/views/LoginView.vue
@@ -0,0 +1,55 @@
+<template>
+<!--  from for login-->
+  <a-form
+      :model="formState"
+      name="basic"
+      :label-col="{ span: 8 }"
+      :wrapper-col="{ span: 16 }"
+      autocomplete="off"
+      @finish="onFinish"
+      @finishFailed="onFinishFailed"
+  >
+    <a-form-item
+        label="phone"
+        name="phone"
+        :rules="[{ required: true, message: 'Please input your phone!' }]"
+    >
+      <a-input v-model:value="formState.phone" />
+    </a-form-item>
+
+    <a-form-item
+        label="Code"
+        name="code"
+        :rules="[{ required: true, message: 'Please input your verification code!' }]"
+    >
+      <a-input-password v-model:value="formState.code" />
+    </a-form-item>
+
+    <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+      <a-button type="primary" html-type="submit">Submit</a-button>
+    </a-form-item>
+  </a-form>
+</template>
+<!--开启组合式API的写法-->
+<script setup>
+//登录相关
+import { reactive } from 'vue';
+const formState = reactive({
+  phone: '',
+  code: '',
+});
+const onFinish = values => {
+  console.log('Success:', values);
+  login_success();
+};
+const onFinishFailed = errorInfo => {
+  console.log('Failed:', errorInfo);
+};
+
+//登录提示
+import { message } from 'ant-design-vue';
+const login_success = () => {
+  message.success('Hello, Welcome to train12306!');
+};
+
+</script>
